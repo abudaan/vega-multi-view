a2df05cdcb1bd21402f382f333fd656c5ad8dc18
abudaan <daniel@abumarkub.net>
2017-09-06 14:27:24 +0200

    added more configuration options

--- dist/index.js
+++ dist/index.js
@@ -26,11 +26,20 @@ var _leafletVega2 = _interopRequireDefault(_leafletVega);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var Rx = require('rxjs/Rx');
+
+var mapIndexed = _ramda2.default.addIndex(_ramda2.default.map);
 
 (0, _leafletVega2.default)();
 
-var createLeafletVega = async function createLeafletVega(elem, spec, view) {
+var createLeafletVega = async function createLeafletVega(data, renderer) {
+    var spec = data.spec,
+        view = data.view,
+        runtime = data.runtime,
+        element = data.element;
+
     var signals = spec.signals || [];
     var zoom = _ramda2.default.find(_ramda2.default.propEq('name', 'zoom'))(signals);
     var latitude = _ramda2.default.find(_ramda2.default.propEq('name', 'latitude'))(signals);
@@ -41,7 +50,7 @@ var createLeafletVega = async function createLeafletVega(elem, spec, view) {
         return;
     }
 
-    var map = _leaflet2.default.map(elem, {
+    var map = _leaflet2.default.map(element, {
         zoomAnimation: false
     }).setView([latitude.value, longitude.value], zoom.value);
 
@@ -51,67 +60,59 @@ var createLeafletVega = async function createLeafletVega(elem, spec, view) {
     }).addTo(map);
 
     _leaflet2.default.vega(view, {
-        renderer: spec.runtime.renderer || 'canvas',
+        renderer: runtime.renderer || renderer,
         // Make sure the legend stays in place
         delayRepaint: true
     }).addTo(map);
 };
 
-// const loadSpecs = async (urls: string[]) => {
+/*
+const debug = (data) => {
+    R.forEach((d) => {
+        const {
+            spec,
+            view,
+        } = d;
+        view.addSignalListener(signal.name, (name, data) => {
+            console.log(spec.description, name, data);
+        });
+    }, view.vega.signals || []);
+
+
+    const numDataSources = spec.data.length;
+    let numLoaded = 0;
+    const dataPoller = setInterval(() => {
+        R.forEach((data) => {
+            const loaded = view.data(data.name);
+            if (loaded !== null) {
+                console.log('[DATA]:', spec.description, data.name, loaded);
+                numLoaded += 1;
+            }
+            if (numLoaded === numDataSources) {
+                // console.log('all data loaded');
+                clearInterval(dataPoller);
+            }
+        }, views);
+    }, 10);
+};
+*/
+
 var loadSpecs = async function loadSpecs(urls) {
-    var i = 0;
-    var specs = {};
+    var specs = [];
     await Promise.all(urls.map(async function (url) {
         var spec = await (0, _fetchHelpers.fetchJSON)(url);
-        var id = 'spec_' + i;
-        var view = new _vega.View((0, _vega.parse)(spec));
-        if (spec.runtime && spec.runtime.tooltipOptions) {
-            (0, _vegaTooltip.vega)(view, spec.runtime.tooltipOptions);
-        }
-
-        var debug = false;
-        if (debug) {
-            _ramda2.default.forEach(function (signal) {
-                view.addSignalListener(signal.name, function (name, data) {
-                    console.log(spec.description, name, data);
-                });
-            }, spec.signals || []);
-
-            var numDataSources = spec.data.length;
-            var numLoaded = 0;
-            var dataPoller = setInterval(function () {
-                _ramda2.default.forEach(function (data) {
-                    var loaded = view.data(data.name);
-                    if (loaded !== null) {
-                        console.log('[DATA]:', spec.description, data.name, loaded);
-                        numLoaded += 1;
-                    }
-                    if (numLoaded === numDataSources) {
-                        // console.log('all data loaded');
-                        clearInterval(dataPoller);
-                    }
-                }, spec.data || []);
-            }, 10);
-        }
-
-        specs[id] = {
-            id: id,
-            spec: spec,
-            view: view
-        };
-        i += 1;
+        specs.push(spec);
     }));
     return specs;
 };
 
-var createStream = function createStream(data) {
-    var spec = data.spec,
+var publishSignal = function publishSignal(data) {
+    var runtime = data.runtime,
         view = data.view;
-
 
     var streams = {};
 
-    if (_ramda2.default.isNil(spec.runtime) || _ramda2.default.isNil(spec.runtime.publish)) {
+    if (_ramda2.default.isNil(runtime.publish)) {
         return streams;
     }
 
@@ -125,17 +126,18 @@ var createStream = function createStream(data) {
         } catch (e) {
             console.error(e.message);
         }
-    }, spec.runtime.publish);
+    }, runtime.publish);
 
     return streams;
 };
 
-var subscribeToStream = function subscribeToStream(data, streams) {
-    var spec = data.spec,
-        view = data.view;
+var subscribeToSignal = function subscribeToSignal(data, streams) {
+    var view = data.view,
+        spec = data.spec,
+        runtime = data.runtime;
 
 
-    if (_ramda2.default.isNil(spec.runtime) || _ramda2.default.isNil(spec.runtime.subscribe)) {
+    if (_ramda2.default.isNil(runtime.subscribe)) {
         return;
     }
 
@@ -153,65 +155,137 @@ var subscribeToStream = function subscribeToStream(data, streams) {
             // console.log(subscribe.as, value);
             view.signal(subscribe.as, value).run();
         });
-    }, spec.runtime.subscribe);
+    }, runtime.subscribe);
 };
 
-var addViews = function addViews(specs, divs) {
-    specs.forEach(function (d, i) {
-        var id = d.id,
-            spec = d.spec,
-            view = d.view;
+var addViews = function addViews(data, renderer) {
+    data.forEach(function (d, i) {
+        var view = d.view,
+            runtime = d.runtime,
+            element = d.element;
 
-        var div = divs[i];
-        if (spec.runtime.leaflet === true) {
-            createLeafletVega(div, spec, view);
+        if (runtime.leaflet === true) {
+            createLeafletVega(d, renderer);
         } else {
-            view.renderer(spec.runtime.renderer || 'canvas').initialize('#' + id);
+            view.renderer(runtime.renderer || renderer).initialize(element);
         }
     });
 };
 
-var addDivs = function addDivs(specs, container, cssClass) {
-    var divs = [];
-    specs.forEach(function (d) {
-        var elem = document.createElement('div');
-        var id = d.id,
-            spec = d.spec;
-
-        elem.id = id;
-        elem.className = cssClass;
-        elem.style.width = spec.width + 'px';
-        elem.style.height = spec.height + 'px';
-        container.appendChild(elem);
-        divs.push(elem);
+var addTooltips = function addTooltips(data) {
+    data.forEach(function (d) {
+        if (typeof d.runtime.tooltipOptions !== 'undefined') {
+            (0, _vegaTooltip.vega)(d.view, d.runtime.tooltipOptions);
+        }
     });
-    return divs;
 };
 
-var createViews = function createViews(data) {
-    var container = data.container,
-        cssClass = data.cssClass,
-        urls = data.urls;
+var connectSignals = function connectSignals(data) {
+    var streams = {};
+    _ramda2.default.forEach(function (d) {
+        streams = _extends({}, streams, publishSignal(d));
+    }, _ramda2.default.values(data));
 
+    _ramda2.default.forEach(function (d) {
+        subscribeToSignal(d, streams);
+    }, _ramda2.default.values(data));
+};
+
+var addElements = function addElements(data, container, className) {
+    return _ramda2.default.map(function (d) {
+        var element = d.runtime.element;
+        if (_ramda2.default.isNil(element) === false) {
+            if (typeof element === 'string') {
+                element = document.getElementById(d.runtime.element);
+                if (_ramda2.default.isNil(element)) {
+                    console.error('element "' + d.runtime.element + '" could not be found');
+                    return _extends({}, d, {
+                        element: null
+                    });
+                }
+            } else if (element instanceof HTMLElement !== true) {
+                console.error('element "' + d.runtime.element + '" is not a valid HTMLElement');
+                return _extends({}, d, {
+                    element: null
+                });
+            }
+        } else {
+            element = document.createElement('div');
+            element.id = d.id;
+            if (typeof d.className === 'string') {
+                element.className = d.className;
+            } else if (typeof className === 'string') {
+                element.className = className;
+            }
+        }
+
+        element.style.width = d.spec.width + 'px';
+        element.style.height = d.spec.height + 'px';
+        container.appendChild(element);
+        return _extends({}, d, {
+            element: element
+        });
+    }, data);
+};
+
+var createViews = function createViews(config) {
+    var _config$container = config.container,
+        container = _config$container === undefined ? document.body : _config$container,
+        specs = config.specs;
+    var _config$className = config.className,
+        className = _config$className === undefined ? false : _config$className,
+        runtimes = config.runtimes,
+        _config$renderer = config.renderer,
+        renderer = _config$renderer === undefined ? 'canvas' : _config$renderer;
+
+
+    if (_ramda2.default.isNil(container)) {
+        container = document.body;
+    }
+
+    if (_ramda2.default.isArrayLike(specs) === false) {
+        specs = [specs];
+    }
+
+    var specUrls = _ramda2.default.filter(function (spec) {
+        return typeof spec === 'string';
+    }, specs);
+    specs = _ramda2.default.filter(function (spec) {
+        return typeof spec !== 'string';
+    }, specs);
 
     return new Promise(function (resolve, reject) {
-        loadSpecs(urls).then(function (specs) {
-            var streams = {};
-            _ramda2.default.forEach(function (spec) {
-                streams = _extends({}, streams, createStream(spec));
-            }, _ramda2.default.values(specs));
-
-            _ramda2.default.forEach(function (spec) {
-                subscribeToStream(spec, streams);
-            }, _ramda2.default.values(specs));
-
-            var specsArray = _ramda2.default.map(function (id) {
-                return specs[id];
-            }, _ramda2.default.keys(specs));
-            var divs = addDivs(specsArray, container, cssClass);
+        var data = void 0;
+        loadSpecs(specUrls).then(function (loadedSpecs) {
+            specs = [].concat(_toConsumableArray(specs), _toConsumableArray(loadedSpecs));
+            data = mapIndexed(function (s, i) {
+                var id = 'spec_' + i;
+                var runtime = {};
+                var spec = _extends({}, s);
+                if (typeof spec.runtime !== 'undefined') {
+                    runtime = _extends({}, spec.runtime);
+                    delete spec.runtime;
+                } else if (_ramda2.default.isNil(runtimes[i]) === false) {
+                    runtime = runtimes[i];
+                }
+                var view = new _vega.View((0, _vega.parse)(spec));
+                return {
+                    id: id,
+                    spec: spec,
+                    view: view,
+                    runtime: runtime
+                };
+            }, specs);
+            data = addElements(data, container, className);
+            addTooltips(data);
+            connectSignals(data);
+            // wait until the next paint cycle so the created elements
+            // are added to the DOM
             setTimeout(function () {
-                addViews(specsArray, divs);
-                resolve('done');
+                addViews(data, renderer);
+                resolve({
+                    data: data
+                });
             }, 0);
         });
     });

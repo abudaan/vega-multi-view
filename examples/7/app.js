71191a2153e8b266e3d85f3299661c6bd461c119
abudaan <daniel@abumarkub.net>
2017-10-01 16:27:58 +0200

    add example 8

--- examples/7/app.js
+++ examples/7/app.js
@@ -1,6 +1,6 @@
 // import { addViews, removeViews } from '../../src/js/index';
 // const vmv = require('../../src/js/index');
-const { addViews, removeViews } = require('../../src/js/index');
+const { addViews, removeViews } = require('../../src/index');
 
 addViews('http://localhost:3000/test-spec/').then((result) => {
     console.log(result);

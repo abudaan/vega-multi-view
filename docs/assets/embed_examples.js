cd68955f116e8c382a38616cd9a5c847ca4c0aed
abudaan <abudaan@gmail.com>
2018-01-15 18:17:23 +0100

    delay examples

--- docs/assets/embed_examples.js
+++ docs/assets/embed_examples.js
@@ -1,14 +1,17 @@
-const examples = document.getElementsByClassName('example');
-const addMultipleConfigs = window.vmv.addMultipleConfigs;
 
-const configUrls = [];
-Array.from(examples).forEach((example) => {
-    configUrls.push(example.dataset.url);
-});
-
-addMultipleConfigs(configUrls)
-    .then((results) => {
-        console.log(results);
-    }).catch((errors) => {
-        console.error(errors);
+document.onload = function () {
+    const addMultipleConfigs = window.vmv.addMultipleConfigs;
+    const examples = document.getElementsByClassName('example');
+    const configUrls = [];
+    Array.from(examples).forEach((example) => {
+        configUrls.push(example.dataset.url);
     });
+    console.log('embedding examples');
+    addMultipleConfigs(configUrls)
+        .then((results) => {
+            console.log(results);
+        }).catch((errors) => {
+            console.error(errors);
+        });
+};
+
